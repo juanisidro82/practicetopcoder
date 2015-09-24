b41686ff1fbe624c56150319d6c675effcae0326
juansidro82 <juanisidro82@yahoo.com.mx>
2015-09-24 12:46:35 -0500

    refactoring the file, for adapt to next test

--- SRM 144/test.R
+++ SRM 144/test.R
@@ -7,15 +7,21 @@ objecto = new(nombreclase)
 testarchivo = readLines(paste(nombreclase, ".sample", sep=""))
 entrada = ""
 salida = ""
-for (i in 1:length(testarchivo)) {
+lineastestarchivo = length(testarchivo)
+for (i in 1:lineastestarchivo) {
     if (substring(testarchivo[i], 1, 10) != "-- Example") next
+    indicelineavacia = i + 1
+    indicelineafin = i + 1
+    while(substring(testarchivo[indicelineafin], 1, 10) != "-- Example" && indicelineafin <= lineastestarchivo) {
+        if (testarchivo[indicelineafin] == "") indicelineavacia = indicelineafin
+        indicelineafin = indicelineafin + 1
+    }
     numeroprueba = testarchivo[i]
-    entrada = testarchivo[i+1]
-    salida =  testarchivo[i+3]
+    entrada = testarchivo[(i+1):(indicelineavacia - 1)]
+    salida =  testarchivo[(indicelineavacia + 1):(indicelineafin - 1)]
     texto = paste("objecto@", names(tipoclases)[1], " = ", entrada, sep ="")
     eval(parse(text=texto))
     resultados = eval(call(definicion, objecto))
-    print(resultados)
     if (checkEquals(salida, resultados))  {
       print(numeroprueba)
       print("passed")
